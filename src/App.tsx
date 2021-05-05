fd940327d133b5323968f58b162175be5e67f039
BMidhun <midhunbabygeorge@gmail.com>
2021-05-05 18:04:45 +0530

    ESLINT and Prettier configuration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import SpiderImage from './spider.png' // Typescript doesn't understand this module. So we have to declare the module under .d.ts file
-import KiwiSvg from './kiwi.svg';
+import KiwiSvg from './kiwi.svg'
 
-console.log(process.env);
-const App = () => <div>
-    <h1>I'm on {process.env.name}</h1>
-    <img src={SpiderImage} alt="Spider Img"/>
-    <img src={KiwiSvg} alt = "Kiwi.svg" />
-</div>
+console.log(process.env)
 
-export default App;
+const App = () => (
+  <div>
+    <h1>I&apos;m on {process.env.name}</h1>
+    <img src={SpiderImage} alt="Spider Img" />
+    <img src={KiwiSvg} alt="Kiwi.svg" />
+  </div>
+)
+
+export default App
