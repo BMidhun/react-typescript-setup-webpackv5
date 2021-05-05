38cded989df5ec542701fc955526e2171d2a3978
BMidhun <midhunbabygeorge@gmail.com>
2021-05-05 19:27:19 +0530

    General Settings added

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,18 +1,22 @@
 const webpack = require("webpack");
 const path = require("path");
-const envPath = path.resolve(__dirname,"../",".env");
+const envPath = path.resolve(__dirname, "../", ".env");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 const dotenv = require("dotenv");
 
-const envparsed = dotenv.config({path:envPath}).parsed;
+const envparsed = dotenv.config({ path: envPath }).parsed;
 
 module.exports = {
-    mode:'production',
-    devtool : 'source-map', // OPTIONAL
-    plugins : [
-        new webpack.DefinePlugin({
-            "process.env" : JSON.stringify(envparsed)
-        })
-    ]
-}
+  mode: "production",
+  devtool: "source-map", // OPTIONAL
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(envparsed),
+    }),
 
+    // new BundleAnalyzerPlugin(), // Uncomment to see a server that shows the modules that are part of the bundle
+  ],
+};
